782d495465878d18bfec343b71e3ea5a00727362
Harrison Allen <harrisonallen555@gmail.com>
2021-07-17 00:57:05 -0400

    Full road rendered

--- pebble-gbc-graphics/src/c/graphics/road.c
+++ pebble-gbc-graphics/src/c/graphics/road.c
@@ -7,8 +7,8 @@ static void load_palettes(GBC_Graphics *graphics) {
     GBC_Graphics_set_bg_palette(graphics, TREE_ROAD_PALETTE, GColorBlackARGB8, GColorDarkGrayARGB8, GColorDarkGreenARGB8, GColorMayGreenARGB8);
     GBC_Graphics_set_bg_palette(graphics, ROAD_PALETTE, GColorBlackARGB8, GColorDarkGrayARGB8, GColorLightGrayARGB8, GColorYellowARGB8);
 #else
-    GBC_Graphics_set_bg_palette(graphics, GROUND_PALETTE, GBC_WHITE, GBC_BLACK, GBC_BLACK, GBC_GRAY);
-    GBC_Graphics_set_bg_palette(graphics, TREE_ROAD_PALETTE, GBC_BLACK, GBC_BLACK, GBC_BLACK, GBC_GRAY);
+    GBC_Graphics_set_bg_palette(graphics, GROUND_PALETTE, GBC_WHITE, GBC_BLACK, GBC_BLACK, GBC_WHITE);
+    GBC_Graphics_set_bg_palette(graphics, TREE_ROAD_PALETTE, GBC_BLACK, GBC_BLACK, GBC_BLACK, GBC_WHITE);
     GBC_Graphics_set_bg_palette(graphics, ROAD_PALETTE, GBC_BLACK, GBC_BLACK, GBC_GRAY, GBC_WHITE);
 #endif
 }
@@ -21,15 +21,65 @@ void draw_road(GBC_Graphics *graphics, uint8_t road_vram_offset) {
 
     // Fill in the base for the road
     for (uint8_t x = 0; x < LAYER_WIDTH; x++) { 
+        GBC_Graphics_bg_set_tile_and_attrs(graphics, x, LAYER_HEIGHT - 5, SKY_TILE, ground_attrs);
         GBC_Graphics_bg_set_tile_and_attrs(graphics, x, LAYER_HEIGHT - 4, road_vram_offset + HORIZON, ground_attrs);
         GBC_Graphics_bg_set_tile_and_attrs(graphics, x, LAYER_HEIGHT - 3, road_vram_offset + ROAD_TOP, road_attrs);
         GBC_Graphics_bg_set_tile_and_attrs(graphics, x, LAYER_HEIGHT - 2, road_vram_offset + ROAD_BOT, road_attrs);
         GBC_Graphics_bg_set_tile_and_attrs(graphics, x, LAYER_HEIGHT - 1, GROUND_TILE, ground_attrs);
     }
 
-    // Place trees
+    // Place upper trees
+    uint8_t tile_odds;
+    for (uint8_t x = 0; x < LAYER_WIDTH; x++) {
+        tile_odds = rand()%4;
+        switch (tile_odds) {
+            case 0:
+                GBC_Graphics_bg_set_tile_and_attrs(graphics, x, LAYER_HEIGHT - 5, road_vram_offset + TREE_TOP, ground_attrs);
+                GBC_Graphics_bg_set_tile_priority(graphics, x, LAYER_HEIGHT - 5, true);
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 4, road_vram_offset + TREE_BOT_SKY);
+                x++;
+                break; 
+            case 1:
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 4, road_vram_offset + TREE_SMALL_SKY);
+                x++;
+                break;
+            default:
+                break;
+        }
+    }
 
     // Place cars
-    
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "road generated");
+    for (uint8_t x = 0; x < LAYER_WIDTH; x++) {
+        tile_odds = rand()%4;
+        switch (tile_odds) {
+            case 0:
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 3, road_vram_offset + CAR_TOP_LEFT);
+                GBC_Graphics_bg_set_tile(graphics, x + 1, LAYER_HEIGHT - 3, road_vram_offset + CAR_TOP_RIGHT);
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 2, road_vram_offset + CAR_BOT_LEFT);
+                GBC_Graphics_bg_set_tile(graphics, x + 1, LAYER_HEIGHT - 2, road_vram_offset + CAR_BOT_RIGHT);
+                x += 2;
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Place lower trees
+    for (uint8_t x = 0; x < LAYER_WIDTH; x++) {
+        tile_odds = rand()%4;
+        switch (tile_odds) {
+            case 0:
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 2, road_vram_offset + TREE_TOP);
+                GBC_Graphics_bg_set_tile_palette(graphics, x, LAYER_HEIGHT - 2, TREE_ROAD_PALETTE);
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 1, road_vram_offset + TREE_BOT_GROUND);
+                x++;
+                break; 
+            case 1:
+                GBC_Graphics_bg_set_tile(graphics, x, LAYER_HEIGHT - 1, road_vram_offset + TREE_SMALL_GROUND);
+                x++;
+                break;
+            default:
+                break;
+        }
+    }
 }
