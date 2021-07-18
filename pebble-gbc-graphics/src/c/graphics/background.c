e9fa05584879e0a1148a36cea22db975b762f6a9
Harrison Allen <hmallen@mit.edu>
2021-07-18 13:54:34 -0400

    slowed bg scroll, commented interrupts, added get_bg_scroll

--- pebble-gbc-graphics/src/c/graphics/background.c
+++ pebble-gbc-graphics/src/c/graphics/background.c
@@ -157,36 +157,55 @@ static void animate_window(GBC_Graphics *graphics) {
 
 
 static void handle_scroll_interrupt(GBC_Graphics *graphics) {
-  int8_t tree_position = s_bg_scroll_y - (s_bg_max_scroll_y - TILE_HEIGHT * 5);
-  int8_t road_position = s_bg_scroll_y - (s_bg_max_scroll_y - TILE_HEIGHT * 3);
-  int8_t bottom_position = s_bg_scroll_y - (s_bg_max_scroll_y - TILE_HEIGHT * 1);
+  int8_t tree_position = s_bg_scroll_y - (s_bg_max_scroll_y - TILE_HEIGHT * 5);   // The position of the trees on top of the road
+  int8_t road_position = s_bg_scroll_y - (s_bg_max_scroll_y - TILE_HEIGHT * 3);   // The position of the top of the road
+  int8_t bottom_position = s_bg_scroll_y - (s_bg_max_scroll_y - TILE_HEIGHT * 1); // The position of the trees below the road
   if (GBC_Graphics_stat_get_line_y_compare(graphics) == 0) {
+    // Move to top left of background where the score bar resides
     GBC_Graphics_bg_set_scroll_pos(graphics, 0, 0);
-    GBC_Graphics_stat_set_line_y_compare(graphics, 8);
-  } else if (GBC_Graphics_stat_get_line_y_compare(graphics) == 8) {
+
+    // Interrupt on the end of the score bar
+    GBC_Graphics_stat_set_line_y_compare(graphics, 8); 
+  } else if (GBC_Graphics_stat_get_line_y_compare(graphics) == 8) { 
+    // Done drawing score bar, return to the current scroll position
     GBC_Graphics_bg_set_scroll_pos(graphics, s_bg_scroll_x, s_bg_scroll_y);
-    if (tree_position > 0) {
+
+    if (tree_position > 0) { 
+      // Top row of trees is visible, so we want to interrupt on that position
       GBC_Graphics_stat_set_line_y_compare(graphics, GBC_Graphics_get_screen_height(graphics) - tree_position);
-    } else {
+    } else { 
+      // No trees visible, interrupt on next pass to the score bar
       GBC_Graphics_stat_set_line_y_compare(graphics, 0);
     }
-  } else {
+  } else { 
+    // We're drawing one of the features on the bottom
     if (GBC_Graphics_stat_get_line_y_compare(graphics) == GBC_Graphics_get_screen_height(graphics) - tree_position) {
+      // Move the top trees twice as fast as the sky
       GBC_Graphics_bg_set_scroll_x(graphics, s_bg_scroll_x * 2);
+
       if (road_position > 0) {
+        // Road is visible, interrupt on that position
         GBC_Graphics_stat_set_line_y_compare(graphics, GBC_Graphics_get_screen_height(graphics) - road_position);
       } else {
+        // No road visible, interrupt on next pass to the score bar
         GBC_Graphics_stat_set_line_y_compare(graphics, 0);
       }
     } else if (GBC_Graphics_stat_get_line_y_compare(graphics) == GBC_Graphics_get_screen_height(graphics) - road_position) {
+      // Move the road three times as fast as the sky
       GBC_Graphics_bg_set_scroll_x(graphics, s_bg_scroll_x * 3);
+
       if (bottom_position > 0) {
+        // Bottom trees are visible, interrupt on that position
         GBC_Graphics_stat_set_line_y_compare(graphics, GBC_Graphics_get_screen_height(graphics) - bottom_position);
       } else {
+        // No road visible, interrupt on next pass to the score bar
         GBC_Graphics_stat_set_line_y_compare(graphics, 0);
       }
     } else {
+      // Move the bottom trees four times as fast as the sky
       GBC_Graphics_bg_set_scroll_x(graphics, s_bg_scroll_x * 4);
+
+      // Interrupt on next pass to the score bar
       GBC_Graphics_stat_set_line_y_compare(graphics, 0);
     }
   } 
@@ -196,7 +215,7 @@ void render_background(GBC_Graphics *graphics, uint player_x, uint8_t player_y) 
   // Here, I explicity set the scroll pos based on the player pos.
   // However, you could use GBC_Graphics_bg_move() to move the background, like I do with the window
   s_bg_scroll_y = clamp_int(0, player_y - (GBC_Graphics_get_screen_height(graphics) / 2), s_bg_max_scroll_y);
-  s_bg_scroll_x = (uint8_t)player_x;
+  s_bg_scroll_x = (uint8_t)(player_x / 2);
 
   
   GBC_Graphics_set_line_compare_interrupt_callback(graphics, handle_scroll_interrupt);
@@ -213,4 +232,12 @@ void animate_graphics(GBC_Graphics *graphics) {
 
 void start_window_animation() {
   s_animate_window_tiles = true;
+}
+
+uint8_t get_bg_scroll_x() {
+  return s_bg_scroll_x;
+}
+
+uint8_t get_bg_scroll_y() {
+  return s_bg_scroll_y;
 }
--- pebble-gbc-graphics/src/c/graphics/background.h
+++ pebble-gbc-graphics/src/c/graphics/background.h
@@ -19,3 +19,5 @@ void start_bg_animation();
 void start_window_animation();
 void change_bg_scroll();
 void render_background(GBC_Graphics *graphics, uint player_x, uint8_t player_y);
+uint8_t get_bg_scroll_x();
+uint8_t get_bg_scroll_y();
