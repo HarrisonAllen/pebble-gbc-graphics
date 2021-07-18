175428e5233a38a4abadde6548c936aeb168b431
Harrison Allen <hmallen@mit.edu>
2021-07-18 00:35:19 -0400

    implemented balloon and fuel characters

--- pebble-gbc-graphics/src/c/graphics/text.c
+++ pebble-gbc-graphics/src/c/graphics/text.c
@@ -1,12 +1,27 @@
 #include "text.h"
 #include "../util.h"
 
-extern uint8_t text_vram_offset;
+extern uint8_t text_vram_offset, sprites_vram_offset;
 
-void clear_top_row(GBC_Graphics *graphics) {
+static void setup_text_palettes(GBC_Graphics *graphics) {
+#if defined(PBL_COLOR)
+    GBC_Graphics_set_bg_palette(graphics, BALLOON_TEXT_PALETTE, GColorPictonBlueARGB8, GColorBlackARGB8, GColorRedARGB8, GColorWhiteARGB8);
+    GBC_Graphics_set_bg_palette(graphics, FUEL_TEXT_PALETTE, GColorPictonBlueARGB8, GColorBlackARGB8, GColorIslamicGreenARGB8, GColorWhiteARGB8);
+#else
+    GBC_Graphics_set_bg_palette(graphics, BALLOON_TEXT_PALETTE, GBC_WHITE, GBC_BLACK, GBC_GRAY, GBC_WHITE);
+    GBC_Graphics_set_bg_palette(graphics, FUEL_TEXT_PALETTE, GBC_WHITE, GBC_BLACK, GBC_GRAY, GBC_WHITE);
+#endif
+}
+
+static void clear_top_row(GBC_Graphics *graphics) {
     for (uint8_t x = 0; x < TILEMAP_WIDTH; x++) {
-        GBC_Graphics_bg_set_tile_and_attrs(graphics, x, 0, BLANK_TILE, GBC_Graphics_attr_make(0, 0, false, false, false));
+        GBC_Graphics_bg_set_tile_and_attrs(graphics, x, 0, BLANK_TILE, GBC_Graphics_attr_make(0, 0, false, false, true));
     }
+}
+
+void text_init(GBC_Graphics *graphics) {
+    setup_text_palettes(graphics);
+    clear_top_row(graphics);
 }
 
 void draw_text_at_location(GBC_Graphics *graphics, char *text, uint8_t x, uint8_t y, uint8_t palette_num) {
@@ -15,6 +30,7 @@ void draw_text_at_location(GBC_Graphics *graphics, char *text, uint8_t x, uint8_
     uint char_index = 0;
     char cur_char = text[char_index];
     while (cur_char != '\0') {
+        GBC_Graphics_bg_set_tile_palette(graphics, x, y, palette_num);
         if (cur_char >= 'A' && cur_char <= 'Z') {
             GBC_Graphics_bg_set_tile(graphics, x, y, text_vram_offset + (cur_char - 'A'));
         } else if (cur_char >= '0' && cur_char <= '9') {
@@ -24,10 +40,17 @@ void draw_text_at_location(GBC_Graphics *graphics, char *text, uint8_t x, uint8_
         } else if (cur_char == '\n') {
             x = start_x;
             y++;
+        } else if (cur_char == 'x') {
+            GBC_Graphics_bg_set_tile(graphics, x, y, text_vram_offset + TIMES_OFFSET);
+        }else if (cur_char == 'b') {
+            GBC_Graphics_bg_set_tile(graphics, x, y, sprites_vram_offset + BALLOON_TILE);
+            GBC_Graphics_bg_set_tile_palette(graphics, x, y, BALLOON_TEXT_PALETTE);
+        }else if (cur_char == 'f') {
+            GBC_Graphics_bg_set_tile(graphics, x, y, sprites_vram_offset + FUEL_TILE);
+            GBC_Graphics_bg_set_tile_palette(graphics, x, y, FUEL_TEXT_PALETTE);
         } else {
             GBC_Graphics_bg_set_tile(graphics, x, y, BLANK_TILE);
         }
-        GBC_Graphics_bg_set_tile_palette(graphics, x, y, palette_num);
         x++;
         char_index++;
         cur_char = text[char_index];
--- pebble-gbc-graphics/src/c/graphics/text.h
+++ pebble-gbc-graphics/src/c/graphics/text.h
@@ -3,9 +3,16 @@
 #include <pebble.h>
 #include "../pebble-gbc-graphics/pebble-gbc-graphics.h"
 
-#define BLANK_TILE 0
+#define BLANK_TILE 1
 #define NUMBER_OFFSET 26
 #define COLON_OFFSET 36
+#define TIMES_OFFSET 37
 
-void clear_top_row(GBC_Graphics *graphics);
+#define BALLOON_TILE 18
+#define FUEL_TILE 23
+
+#define BALLOON_TEXT_PALETTE 4
+#define FUEL_TEXT_PALETTE 5
+
+void text_init(GBC_Graphics *graphics);
 void draw_text_at_location(GBC_Graphics *graphics, char *text, uint8_t x, uint8_t y, uint8_t palette_num);
