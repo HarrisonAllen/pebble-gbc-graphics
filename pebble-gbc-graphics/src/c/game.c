2ea7880f23f01c222e10c47d3dcde14ec83c020f
Harrison Allen <hmallen@mit.edu>
2021-07-18 13:51:24 -0400

    Implemented collision, changed screen bounds

--- pebble-gbc-graphics/src/c/game.c
+++ pebble-gbc-graphics/src/c/game.c
@@ -8,7 +8,28 @@
 static AppTimer *s_frame_timer; // Timer to animate the frames
 static GBC_Graphics *s_graphics; // Need to keep a reference to the graphics object
 static uint16_t player_score;
-static uint8_t player_fuel;
+static uint16_t player_fuel;
+
+static void check_collision() {
+  for (uint8_t i = 0; i < NUMBER_OF_ITEMS; i++) {
+    bool item_overlaps_player = grects_overlap(get_player_collision(), get_item_collision(i));
+    if (item_overlaps_player) {
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Overlap! Item %d: Player (%d, %d), Item (%d, %d)", i, get_player_collision().origin.x, get_player_collision().origin.y, get_item_collision(i).origin.x, get_item_collision(i).origin.y);
+      uint *item = get_item(i);
+      switch(item[0]) {
+        case BALLOON_ID:
+          player_score++;
+          break;
+        case FUEL_ID:
+          player_fuel += 5;
+          break;
+        default:
+          break;
+      }
+      handle_collision(s_graphics, i);
+    }
+  }
+}
 
 static void game_step() {
   player_step(s_graphics);
@@ -16,11 +37,11 @@ static void game_step() {
   items_step(s_graphics);
   animate_graphics(s_graphics);
 
-  // TODO: Add collision, only on the body and nose of the plane
+  check_collision();
 
   char top_banner_text[33];
-  snprintf(top_banner_text, 32, "bx001 f  ");
-  draw_text_at_location(s_graphics, top_banner_text, PBL_IF_ROUND_ELSE(8, 5), 0, 0);
+  snprintf(top_banner_text, 32, "bx%04d f%03d", player_score, player_fuel);
+  draw_text_at_location(s_graphics, top_banner_text, PBL_IF_ROUND_ELSE(6, 3), 0, 0);
 }
 
 static void frame_timer_handle(void* context) {
@@ -48,12 +69,29 @@ GBC_Graphics *init_gbc_graphics(Window *window) {
   // Create with 1 vram bank since that's all the space we'll need
   GBC_Graphics *graphics = GBC_Graphics_ctor(window, 1);
 
-  // This will fill a good portion of the screen on all pebbles, and
-  // have a decent frame rate. I prefer SCREEN_BOUNDS_SQUARE for the best
-  // compromise between size and frame rate. You can also define your own bounds!
-  GBC_Graphics_set_screen_bounds(graphics, SCREEN_BOUNDS_LARGE);
+  // I have predefined some common screen sizes, prefixed by SCREEN_BOUNDS_,
+  // but you can create your own with any resolution!
+  // Keep in mind that drawing fewer pixels (i.e. smaller resolutions) will
+  // lead to some serious performance gains
+#if defined(PBL_ROUND)
+  // For round, I want to make sure the score bar is visible, so I use
+  // the large screen bounds and scoot it down. In main.c, I set the
+  // window color to the same blue as the sky, in order to give the
+  // illusion of a fullscreen application
+  GRect round_bounds = SCREEN_BOUNDS_LARGE;
+  round_bounds.origin.y += 18;
+  GBC_Graphics_set_screen_bounds(graphics, round_bounds);
+#else
+  // For rectangle watches, we can use fullscreen bounds since
+  // no readability is lost
+  GBC_Graphics_set_screen_bounds(graphics, SCREEN_BOUNDS_FULL);
+#endif
   
-  // One OAM slot will use two tiles
+  // This means one sprite uses two tiles, stacked vertically
+  // The Legend of Zelda: Link's Awakening uses this 8x16 mode,
+  // meaning Link's 16x16 character model only uses 2 sprite slots
+  // Pokemon, however, uses the 8x8 mode and uses 4 sprites slots
+  // to render the 16x16 player model
   GBC_Graphics_lcdc_set_8x16_sprite_mode_enabled(graphics, true);
 
   load_tilesheets(graphics);
