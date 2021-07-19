e94c940de17535229da1b461799dad3edd8fbf4b
Harrison Allen <hmallen@mit.edu>
2021-07-18 21:45:43 -0400

    Implemented window layer and animations

---
+++ pebble-gbc-graphics/src/c/graphics/window.c
@@ -0,0 +1,121 @@
+#include "window.h"
+#include "background.h"
+#include "../util.h"
+
+static uint8_t s_window_anim_frame;
+static bool s_animate_window_tiles = false;
+static bool s_window_up = false;
+static bool s_window_animating;
+static GPoint s_anim_start_pos, s_anim_end_pos;
+
+static const uint8_t s_ease_in_out_percentages[] = {
+  0,
+  2,
+  4,
+  8,
+  16,
+  26,
+  38,
+  50,
+  62,
+  74,
+  84,
+  92,
+  96,
+  98,
+  100
+};
+
+// Place the frame on the window layer and make the window layer visible
+static void setup_window_layer(GBC_Graphics *graphics) {
+  #if defined(PBL_COLOR)
+    GBC_Graphics_set_bg_palette(graphics, WINDOW_PALETTE, GColorPictonBlueARGB8, GColorPastelYellowARGB8, GColorWindsorTanARGB8, GColorBlackARGB8);
+  #else
+    GBC_Graphics_set_bg_palette(graphics, WINDOW_PALETTE, GBC_WHITE, GBC_WHITE, GBC_GRAY, GBC_BLACK);
+  #endif
+
+  // Hide the window layer
+  GBC_Graphics_window_set_offset_pos(graphics, GBC_Graphics_get_screen_width(graphics), GBC_Graphics_get_screen_height(graphics));
+}
+
+
+static void clear_window_layer(GBC_Graphics *graphics) {
+  for (uint8_t x = 0; x < TILEMAP_WIDTH; x++) {
+    for (uint8_t y = 0; y < TILEMAP_HEIGHT; y++) {
+      GBC_Graphics_window_set_tile_and_attrs(graphics, x, y, SOLID_TILE_01, GBC_Graphics_attr_make(WINDOW_PALETTE, 0, false, false, true));
+    }
+  }
+}
+
+void window_init(GBC_Graphics *graphics) {
+    setup_window_layer(graphics);
+    clear_window_layer(graphics);
+}
+
+void set_window_layer_frame(GBC_Graphics *graphics, GRect frame, GPoint origin) {
+  uint8_t left_boundary = frame.origin.x;
+  uint8_t right_boundary = frame.origin.x + frame.size.w;
+  uint8_t upper_boundary = frame.origin.y;
+  uint8_t lower_boundary = frame.origin.y + frame.size.h;
+  
+  // These attributes set a tile to palette WINDOW_PALETTE, vram bank 0, no x flip, no y flip, no priority over sprite
+  uint8_t window_attrs = GBC_Graphics_attr_make(WINDOW_PALETTE, 0, false, false, true);
+
+  // Draw the corners
+  GBC_Graphics_window_set_tile_and_attrs(graphics, left_boundary, upper_boundary, WINDOW_FRAME_CORNER, window_attrs);
+
+  GBC_Graphics_window_set_tile_and_attrs(graphics, right_boundary, upper_boundary, WINDOW_FRAME_CORNER, window_attrs);
+  GBC_Graphics_window_set_tile_x_flip(graphics, right_boundary, upper_boundary, true); // flip the top right corner
+
+  GBC_Graphics_window_set_tile_and_attrs(graphics, left_boundary, lower_boundary, WINDOW_FRAME_CORNER, window_attrs);
+  GBC_Graphics_window_set_tile_y_flip(graphics, left_boundary, lower_boundary, true); // flip the bottom left corner
+
+  GBC_Graphics_window_set_tile_and_attrs(graphics, right_boundary, lower_boundary, WINDOW_FRAME_CORNER, window_attrs);
+  GBC_Graphics_window_set_tile_y_flip(graphics, right_boundary, lower_boundary, true); // flip the bottom right corner
+  GBC_Graphics_window_set_tile_x_flip(graphics, right_boundary, lower_boundary, true); // flip the bottom right corner
+
+  // Draw the edges
+  for (uint8_t x = left_boundary+1; x < right_boundary; x++) {
+    GBC_Graphics_window_set_tile_and_attrs(graphics, x, upper_boundary, WINDOW_FRAME_HORIZONTAL, window_attrs);
+    GBC_Graphics_window_set_tile_and_attrs(graphics, x, lower_boundary, WINDOW_FRAME_HORIZONTAL, window_attrs);
+    GBC_Graphics_window_set_tile_y_flip(graphics, x, lower_boundary, true); // flip the bottom edge
+  }
+  for (uint8_t y = upper_boundary+1; y < lower_boundary; y++) {
+    GBC_Graphics_window_set_tile_and_attrs(graphics, left_boundary, y, WINDOW_FRAME_VERTICAL, window_attrs);
+    
+    GBC_Graphics_window_set_tile_and_attrs(graphics, right_boundary, y, WINDOW_FRAME_VERTICAL, window_attrs);
+    GBC_Graphics_window_set_tile_x_flip(graphics, right_boundary, y, true); // flip the bottom edge
+  }
+
+  // Fill the middle
+  for (uint8_t x = left_boundary+1; x < right_boundary; x++) {
+    for (uint8_t y = upper_boundary+1; y < lower_boundary; y++) {
+      GBC_Graphics_window_set_tile_and_attrs(graphics, x, y, SOLID_TILE_01, window_attrs);
+    }
+  }
+
+  // And move the window to the position
+  GBC_Graphics_window_set_offset_x(graphics, origin.x);
+  GBC_Graphics_window_set_offset_y(graphics, origin.y);
+}
+
+void start_window_animation(GPoint start, GPoint end) {
+  s_anim_start_pos = start;
+  s_anim_end_pos = end;
+  s_window_animating = true;
+  s_window_anim_frame = 0;
+}
+
+void step_window_animation(GBC_Graphics *graphics) {
+  uint8_t animation_percent = s_ease_in_out_percentages[s_window_anim_frame];
+  GBC_Graphics_window_set_offset_x(graphics, lerp(s_anim_start_pos.x, s_anim_end_pos.x, animation_percent));
+  GBC_Graphics_window_set_offset_y(graphics, lerp(s_anim_start_pos.y, s_anim_end_pos.y, animation_percent));
+  GBC_Graphics_render(graphics);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Animating on frame %d at %d%% to (%d, %d)", s_window_anim_frame, animation_percent, lerp(s_anim_start_pos.x, s_anim_end_pos.x, animation_percent), lerp(s_anim_start_pos.y, s_anim_end_pos.y, animation_percent));
+  s_window_anim_frame++;
+  s_window_animating = animation_percent < 100;
+}
+
+bool is_window_animating() {
+  return s_window_animating;
+}
---
+++ pebble-gbc-graphics/src/c/graphics/window.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <pebble.h>
+#include "../pebble-gbc-graphics/pebble-gbc-graphics.h"
+
+#define WINDOW_FRAME_CORNER 4
+#define WINDOW_FRAME_HORIZONTAL 5
+#define WINDOW_FRAME_VERTICAL 6
+
+#define WINDOW_PALETTE 7
+
+void window_init(GBC_Graphics *graphics);
+void set_window_layer_frame(GBC_Graphics *graphics, GRect frame, GPoint origin);
+void start_window_animation(GPoint start, GPoint end);
+void step_window_animation(GBC_Graphics *graphics);
+bool is_window_animating();
