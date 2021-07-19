36f34fc5cf29ed38e60459aa7e0f2afcaf834785
Harrison Allen <hmallen@mit.edu>
2021-07-18 21:45:28 -0400

    Moved window functions to its own file

--- pebble-gbc-graphics/src/c/graphics/background.h
+++ pebble-gbc-graphics/src/c/graphics/background.h
@@ -7,16 +7,9 @@
 #define SOLID_TILE_01 1
 #define SOLID_TILE_10 2
 #define SOLID_TILE_11 3
-#define WINDOW_FRAME_CORNER 4
-#define WINDOW_FRAME_HORIZONTAL 5
-#define WINDOW_FRAME_VERTICAL 6
-
-#define WINDOW_PALETTE 7
 
 void generate_new_game_background(GBC_Graphics *graphics);
-void animate_graphics(GBC_Graphics *graphics);
 void start_bg_animation();
-void start_window_animation();
 void change_bg_scroll();
 void render_background(GBC_Graphics *graphics, uint player_x, uint8_t player_y);
 uint8_t get_bg_scroll_x();
