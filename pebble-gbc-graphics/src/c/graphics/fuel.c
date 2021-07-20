b0937273a221cf2c5184167466a3ffddcbba9ce2
Harrison Allen <hmallen@mit.edu>
2021-07-20 13:45:45 -0400

    Documented

--- pebble-gbc-graphics/src/c/graphics/fuel.c
+++ pebble-gbc-graphics/src/c/graphics/fuel.c
@@ -1,9 +1,10 @@
 #include "fuel.h"
 #include "../util.h"
 
-extern uint8_t fuel_vram_offset;
+extern uint8_t fuel_vram_offset; // Need to grab the offset of the fuel bar tilesheet
 
 void draw_fuel_bar(GBC_Graphics *graphics, uint16_t fuel_amount, uint16_t max_fuel, uint8_t num_fuel_bars, uint8_t x, uint8_t y) {
+    // Change the color of the bar based on the amount of fuel remaining
 #if defined(PBL_COLOR)
     float fuel_percentage = (float)fuel_amount / max_fuel;
     uint8_t color;
@@ -27,15 +28,20 @@ void draw_fuel_bar(GBC_Graphics *graphics, uint16_t fuel_amount, uint16_t max_fu
     GBC_Graphics_set_bg_palette_color(graphics, FUEL_BAR_PALETTE, 2, color);
 #endif
 
+    // Calculate the number of pixels that should be filled with fuel
     uint8_t num_pixels = (num_fuel_bars * 8) * fuel_amount / max_fuel;
-
     uint8_t num_full_bars = num_pixels / 8;
     uint8_t last_bar_len = num_pixels % 8;
+
     uint8_t attrs = GBC_Graphics_attr_make(FUEL_BAR_PALETTE, 0, false, false, true);
     uint8_t bar_pos;
+    
+    // First, fill up all of the full bars
     for (bar_pos = 0; bar_pos < num_full_bars; bar_pos++) {
         GBC_Graphics_bg_set_tile_and_attrs(graphics, x+bar_pos, y, fuel_vram_offset + FUEL_BAR_FULL, attrs);
     }
+
+    // Then, fill up the bar that is somewhere between full and empty
     if (bar_pos < num_fuel_bars) {
         if (num_full_bars == 0 && last_bar_len == 0 && fuel_amount != 0) {
             GBC_Graphics_bg_set_tile_and_attrs(graphics, x+bar_pos, y, fuel_vram_offset + FUEL_BAR_EMPTY - 1, attrs);
@@ -43,10 +49,14 @@ void draw_fuel_bar(GBC_Graphics *graphics, uint16_t fuel_amount, uint16_t max_fu
             GBC_Graphics_bg_set_tile_and_attrs(graphics, x+bar_pos, y, fuel_vram_offset + FUEL_BAR_EMPTY - last_bar_len, attrs);
         }
     }
+
+    // Next, fill up all of the empty bars
     if (bar_pos < num_fuel_bars) {
         for (bar_pos++; bar_pos < num_fuel_bars; bar_pos++) {
             GBC_Graphics_bg_set_tile_and_attrs(graphics, x+bar_pos, y, fuel_vram_offset + FUEL_BAR_EMPTY, attrs);
         }
     }
+
+    // And finally, place the end cap onto the fuel bar
     GBC_Graphics_bg_set_tile_and_attrs(graphics, x+bar_pos, y, fuel_vram_offset + FUEL_BAR_END, attrs);
 }
--- pebble-gbc-graphics/src/c/graphics/fuel.h
+++ pebble-gbc-graphics/src/c/graphics/fuel.h
@@ -3,10 +3,19 @@
 #include <pebble.h>
 #include "../pebble-gbc-graphics/pebble-gbc-graphics.h"
 
-#define FUEL_BAR_PALETTE 5 // Shared with FUEL_TEXT_PALETTE
+#define FUEL_BAR_FULL 0 // The tile for a full fuel bar
+#define FUEL_BAR_EMPTY 8 // The tile for an empty fuel bar
+#define FUEL_BAR_END 9 // The tile for the end of the fuel bar
+#define FUEL_BAR_PALETTE 5 // The palette for the fuel bar, shared with FUEL_TEXT_PALETTE
 
-#define FUEL_BAR_FULL 0
-#define FUEL_BAR_EMPTY 8
-#define FUEL_BAR_END 9
-
+/**
+ * Draws a fuel bar with num_fuel_bar sections, filled up to fuel_amount/max_fuel
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ * @param fuel_amount The amount of fuel that should be drawn
+ * @param max_fuel The amount of fuel at the end of the bar
+ * @param num_fuel_bars The number of sections the fuel bar should have
+ * @param x The x position of the fuel bar, in tiles
+ * @param y The y position of the fuel bar, in tiles
+ */
 void draw_fuel_bar(GBC_Graphics *graphics, uint16_t fuel_amount, uint16_t max_fuel, uint8_t num_fuel_bars, uint8_t x, uint8_t y);
