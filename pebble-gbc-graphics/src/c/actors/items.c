5eb3fce6b1640bec47ba8f379382c00172fa27c2
Harrison Allen <hmallen@mit.edu>
2021-07-20 10:51:53 -0400

    Documented

--- pebble-gbc-graphics/src/c/actors/items.c
+++ pebble-gbc-graphics/src/c/actors/items.c
@@ -2,37 +2,61 @@
 #include "player.h"
 #include "../graphics/background.h"
 
-// contains item_type, x_position, y_position
-static uint s_items_data[NUMBER_OF_ITEMS][3];
+static uint s_items_data[NUMBER_OF_ITEMS][3]; // Each index contains an item's type, x position, and y position
 
-extern uint8_t sprites_vram_offset;
+extern uint8_t sprites_vram_offset; // Need to grab the offset of the sprites tilesheet
 
+/**
+ * The sprites for the item types
+ */
 static uint8_t s_item_sprites[] = {
-    0,
-    BALLOON_SPRITE,
-    PLUS_ONE_SPRITE,
-    FUEL_SPRITE,
-    PLUS_FUEL_SPRITE
+    0,                  // IT_NONE: Doesn't render, no sprite
+    BALLOON_SPRITE,     // IT_BALLOON: A balloon with a string
+    PLUS_ONE_SPRITE,    // IT_PLUS_ONE: A '+1'
+    FUEL_SPRITE,        // IT_FUEL: A fuel canister on a parachute
+    PLUS_FUEL_SPRITE    // IT_PLUS_FUEL: A '+F'
 };
 
+/**
+ * The sprite palettes for the item types
+ */
 static uint8_t s_item_palettes[] = {
-    0,
-    BALLOON_PALETTE,
-    BALLOON_PALETTE,
-    FUEL_PALETTE,
-    FUEL_PALETTE
+    0,                  // IT_NONE: Doesn't render, no palette
+    BALLOON_PALETTE,    // IT_BALLOON: Primary color is Red
+    BALLOON_PALETTE,    // IT_PLUS_ONE: Primary color is Red (unused)
+    FUEL_PALETTE,       // IT_FUEL: Primary color is Islamic Green
+    FUEL_PALETTE        // IT_PLUS_FUEL: Primary color is Islamic Green (unused)
 };
 
+/**
+ * Sets the item type to a either IT_BALLOON or IT_FUEL, and moves it off-screen
+ * to the right, will later move on-screen once the player reaches it
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ * @param item_index The item to randomize
+ */
 static void set_item(GBC_Graphics *graphics, uint8_t item_index) {
     uint8_t item_odds = (rand() % NUMBER_OF_ITEMS);
-    s_items_data[item_index][0] = (item_odds < BALLOON_ODDS) ? IT_BALLOON : IT_FUEL;
-    s_items_data[item_index][1] = get_player_x() + GBC_Graphics_get_screen_width(graphics) + rand() % (GBC_Graphics_get_screen_width(graphics) * 3);
-    s_items_data[item_index][2] = MIN_PLAYER_Y + rand() % (MAX_PLAYER_Y - MIN_PLAYER_Y);
-    uint8_t item_attrs = GBC_Graphics_attr_make(s_item_palettes[s_items_data[item_index][0]], 0, false, false, false);
-    GBC_Graphics_oam_set_sprite(graphics, ITEM_SPRITE_OFFSET + item_index, 0, 0, sprites_vram_offset + s_item_sprites[s_items_data[item_index][0]], item_attrs);
+    uint* item = s_items_data[item_index];
+    item[0] = (item_odds < NUMBER_OF_BALLOONS) ? IT_BALLOON : IT_FUEL;
+    item[1] = get_player_x() + GBC_Graphics_get_screen_width(graphics) + rand() % (GBC_Graphics_get_screen_width(graphics) * 3);
+    item[2] = MIN_PLAYER_Y + rand() % (MAX_PLAYER_Y - MIN_PLAYER_Y);
+
+    uint8_t item_attrs = GBC_Graphics_attr_make(s_item_palettes[item[0]], 0, false, false, false);
+    GBC_Graphics_oam_set_sprite(graphics, ITEM_OAM_OFFSET + item_index, 0, 0, sprites_vram_offset + s_item_sprites[item[0]], item_attrs);
 }
 
+/**
+ * Initializes the sprite palettes for the items
+ */
 static void setup_item_palettes(GBC_Graphics *graphics) {
+    /**
+     * The colors are:
+     *   1. Transparent (doesn't matter)
+     *   2. Border color
+     *   3. Primary color
+     *   4. Detail color
+     */
 #if defined(PBL_COLOR)
     GBC_Graphics_set_sprite_palette(graphics, BALLOON_PALETTE, GColorPictonBlueARGB8, GColorBlackARGB8, GColorRedARGB8, GColorWhiteARGB8);
     GBC_Graphics_set_sprite_palette(graphics, FUEL_PALETTE, GColorPictonBlueARGB8, GColorBlackARGB8, GColorIslamicGreenARGB8, GColorIslamicGreenARGB8);
@@ -49,30 +73,32 @@ void items_init(GBC_Graphics *graphics) {
     }
 }
 
-static bool is_item_on_screen(GBC_Graphics *graphics, uint8_t item_index) {
-    int item_screen_x = s_items_data[item_index][1] - get_player_x();
-    int item_y = s_items_data[item_index][2] - get_player_y();
-    return false;
-}
-
+/**
+ * Draws the item sprites
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ */
 static void draw_item_sprites(GBC_Graphics *graphics) {
     for (uint8_t i = 0; i < NUMBER_OF_ITEMS; i++) {
-        if (s_items_data[i][0] == IT_NONE) {
-            GBC_Graphics_oam_hide_sprite(graphics, ITEM_SPRITE_OFFSET + i);
+        if (s_items_data[i][0] == IT_NONE) { // Don't render the none type items
+            GBC_Graphics_oam_hide_sprite(graphics, ITEM_OAM_OFFSET + i);
             continue;
         }
+
         int item_screen_y = s_items_data[i][2] - get_bg_scroll_y(graphics) + GBC_SPRITE_OFFSET_Y;
+        // Check if the item is on screen vertically
         if (item_screen_y <= 0 || item_screen_y > (GBC_Graphics_get_screen_height(graphics) + GBC_TILE_HEIGHT * 2)) {
-            GBC_Graphics_oam_hide_sprite(graphics, ITEM_SPRITE_OFFSET + i);
+            GBC_Graphics_oam_hide_sprite(graphics, ITEM_OAM_OFFSET + i);
         } else {
             int item_screen_x = s_items_data[i][1] - get_player_x() + PLAYER_ON_SCREEN_X;
             bool item_overlaps_window = (item_screen_x > GBC_Graphics_window_get_offset_x(graphics) + GBC_SPRITE_OFFSET_X
                 && item_screen_y > GBC_Graphics_window_get_offset_y(graphics) + GBC_SPRITE_OFFSET_Y);
 
+            // Then, check if the item is on screen horizontally and isn't obscured by the window
             if (item_screen_x <= 0 || item_screen_x > (GBC_Graphics_get_screen_width(graphics) + GBC_TILE_WIDTH) || item_overlaps_window) {
-                GBC_Graphics_oam_hide_sprite(graphics, ITEM_SPRITE_OFFSET + i);
+                GBC_Graphics_oam_hide_sprite(graphics, ITEM_OAM_OFFSET + i);
             } else {
-                GBC_Graphics_oam_set_sprite_pos(graphics, ITEM_SPRITE_OFFSET + i, item_screen_x, item_screen_y);
+                GBC_Graphics_oam_set_sprite_pos(graphics, ITEM_OAM_OFFSET + i, item_screen_x, item_screen_y);
             }
         }
     }
@@ -80,7 +106,7 @@ static void draw_item_sprites(GBC_Graphics *graphics) {
 
 GRect get_item_collision(uint8_t item_id) {
     uint *item = s_items_data[item_id];
-    return GRect(item[1], item[2], GBC_TILE_WIDTH, GBC_TILE_HEIGHT * 2);
+    return GRect(item[1], item[2], GBC_TILE_WIDTH, GBC_TILE_HEIGHT * 2); // All items have a 1x2 tile collision
 }
 
 uint *get_item(uint8_t item_id) {
@@ -90,13 +116,13 @@ uint *get_item(uint8_t item_id) {
 void handle_collision(GBC_Graphics *graphics, uint8_t item_id) {
     uint *item = s_items_data[item_id];
     switch (item[0]) {
-        case IT_BALLOON:
+        case IT_BALLOON: // For balloon, replace with plus one
             item[0] = IT_PLUS_ONE;
-            GBC_Graphics_oam_set_sprite_tile(graphics, ITEM_SPRITE_OFFSET + item_id, sprites_vram_offset + s_item_sprites[item[0]]);
+            GBC_Graphics_oam_set_sprite_tile(graphics, ITEM_OAM_OFFSET + item_id, sprites_vram_offset + s_item_sprites[item[0]]);
             break;
-        case IT_FUEL:
+        case IT_FUEL: // For fuel, replace with fuel
             item[0] = IT_PLUS_FUEL;
-            GBC_Graphics_oam_set_sprite_tile(graphics, ITEM_SPRITE_OFFSET + item_id, sprites_vram_offset + s_item_sprites[item[0]]);
+            GBC_Graphics_oam_set_sprite_tile(graphics, ITEM_OAM_OFFSET + item_id, sprites_vram_offset + s_item_sprites[item[0]]);
             break;
         default:
             break;
@@ -106,18 +132,27 @@ void handle_collision(GBC_Graphics *graphics, uint8_t item_id) {
 void items_step(GBC_Graphics *graphics) {
     for (uint8_t i = 0; i < NUMBER_OF_ITEMS; i++) {
         uint *item = s_items_data[i];
+        // Do nothing for none type items
         if (item[0] == IT_NONE) {
             continue;
         }
+
+        // Move item to the left
         item[1] -= 1;
+
+        // For the +1 and +F items, move upwards
         if (item[0] == IT_PLUS_ONE || item[0] == IT_PLUS_FUEL) {
             item[2] -= 1;
         }
+
+        // If the item goes off-screen, re-roll it into a new item
         int item_screen_x = item[1] - get_player_x() + get_player_screen_x();
         if (item_screen_x <= 0 && player_on_screen()) {
             set_item(graphics, i);
         }
     }
+
+    // And don't forget to render!
     draw_item_sprites(graphics);
 }
 
--- pebble-gbc-graphics/src/c/actors/items.h
+++ pebble-gbc-graphics/src/c/actors/items.h
@@ -4,29 +4,74 @@
 #include "../pebble-gbc-graphics/pebble-gbc-graphics.h"
 #include "../util.h"
 
-#define NUMBER_OF_ITEMS 6
-#define BALLOON_ODDS 5
-#define FUEL_ODDS 2
+/**
+ * This class handles the items in the game
+ */
 
-#define ITEM_SPRITE_OFFSET 4
-#define BALLOON_PALETTE 1
-#define FUEL_PALETTE 2
-#define BALLOON_SPRITE 18
-#define PLUS_ONE_SPRITE 20
-#define FUEL_SPRITE 22
-#define PLUS_FUEL_SPRITE 24
+#define NUMBER_OF_ITEMS 6 // The number of items to render at once
+#define NUMBER_OF_BALLOONS 5 // On average, how many of the items should be balloons? The rest will be fuel 
 
+#define ITEM_OAM_OFFSET 4 // The OAM offset for the sprites
+#define BALLOON_SPRITE 18 // The tilesheet offset for the balloon sprite
+#define PLUS_ONE_SPRITE 20 // The tilesheet offset for the +1 sprite
+#define FUEL_SPRITE 22 // The tilesheet offset for the fuel sprite
+#define PLUS_FUEL_SPRITE 24 // The tilesheet offset for the +F sprite
+#define BALLOON_PALETTE 1 // The sprite palette number for the balloon sprites
+#define FUEL_PALETTE 2 // The sprite palette number for the fuel sprites
+
+/**
+ * The different types of items
+ */
 typedef enum {
-    IT_NONE,
-    IT_BALLOON,
-    IT_PLUS_ONE,
-    IT_FUEL,
-    IT_PLUS_FUEL,
+    IT_NONE,        // An empty item, no collision, invisible
+    IT_BALLOON,     // The balloon item, player collects for points, has collision
+    IT_PLUS_ONE,    // The +1 item, appears when balloon is collected, no collision
+    IT_FUEL,        // The fuel item, player collects to replenish fuel, has collision
+    IT_PLUS_FUEL,   // The +F item, appears when fuel is collected, no collision
 } ItemTypes;
 
+/**
+ * Sets up the items
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ */
 void items_init(GBC_Graphics *graphics);
+
+/**
+ * Gets the collision bounding rectangle for an item
+ * 
+ * @param item_id The item to get the collision of
+ * @return The GRect that defines the collision for this item
+ */
 GRect get_item_collision(uint8_t item_id);
+
+/**
+ * Gets the data structure for the item
+ * 
+ * @param item_id The item to get the data of
+ * @return A pointer to a list containing:
+ *  [0] (ItemType) The type of the item
+ *  [1] (uint) The item's x position
+ *  [2] (uint) The item's y position
+ */
 uint *get_item(uint8_t item_id);
+
+/**
+ * Handles collision for the given item
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ * @param item_id The item to handle collision for
+ */
 void handle_collision(GBC_Graphics *graphics, uint8_t item_id);
+
+/**
+ * Steps the items, moving, hiding, generating new
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ */
 void items_step(GBC_Graphics *graphics);
+
+/**
+ * Clears out all of the items
+ */
 void items_clear();
