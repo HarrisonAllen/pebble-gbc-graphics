78bd2c376dbcfd36771d140480f7eeb9891422c6
Harrison Allen <hmallen@mit.edu>
2021-07-20 10:52:09 -0400

    Rearranged for consistency

--- pebble-gbc-graphics/src/c/actors/hiscore.h
+++ pebble-gbc-graphics/src/c/actors/hiscore.h
@@ -10,15 +10,15 @@
  */
 
 
+#define HI_SCORE_OAM_OFFSET 30 // The OAM offset for the sprites
 #define HI_SCORE_SPRITE_1 26 // The tilesheet offset for the first sprite
 #define HI_SCORE_SPRITE_2 28 // The tilesheet offset for the second sprite
 #define HI_SCORE_SPRITE_3 30 // The tilesheet offset for the third sprite
-#define HI_SCORE_PALETTE 3 // The palette for the high score sprites
+#define HI_SCORE_PALETTE 3 // The sprite palette number for the high score sprites
 
 #define HI_SCORE_Y_POS PBL_IF_ROUND_ELSE(62, 93) // The x position relative to the window layer
 #define HI_SCORE_X_POS PBL_IF_ROUND_ELSE(144, 128) // The y position relative to the window layer
 #define HI_SCORE_Y_OFFSET_MAX 3 // The farthest down the object can travel from its origin
-#define HI_SCORE_OAM_OFFSET 30 // The OAM offset for the sprites
 
 /**
  * Initializes the high score object
