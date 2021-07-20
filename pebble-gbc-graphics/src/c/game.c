222c34dd87c89dbf1738d76c415c5644a84e3493
Harrison Allen <hmallen@mit.edu>
2021-07-20 00:49:50 -0400

    Documented

--- pebble-gbc-graphics/src/c/game.c
+++ pebble-gbc-graphics/src/c/game.c
@@ -17,26 +17,37 @@ static GameState s_game_state;
 static uint16_t s_high_score = 0;
 static bool s_new_high_score;
 
+/**
+ * Checks if the player is overlapping any items, and handles the interactions
+ */
 static void check_collision() {
   for (uint8_t i = 0; i < NUMBER_OF_ITEMS; i++) {
     bool item_overlaps_player = grects_overlap(get_player_collision(), get_item_collision(i));
     if (item_overlaps_player) {
       uint *item = get_item(i);
       switch(item[0]) {
+        // Overlap a balloon, that's a point
         case IT_BALLOON:
           s_player_score++;
           break;
+        
+        // Overlap fuel, regain some fuel
         case IT_FUEL:
           s_player_fuel += MAX_PLAYER_FUEL / NUM_FUEL_BARS;
           break;
         default:
           break;
       }
+
+      // Now let the items respond to the collision
       handle_collision(s_graphics, i);
     }
   }
 }
 
+/**
+ * Consumes one unit of fuel, and renders the new fuel bar
+ */
 static void update_fuel() {
   if (s_player_fuel > 0) {
     s_player_fuel--;
@@ -47,26 +58,30 @@ static void update_fuel() {
   draw_fuel_bar(s_graphics, s_player_fuel, MAX_PLAYER_FUEL, NUM_FUEL_BARS, SCORE_BAR_OFFSET + 1, 0);
 }
 
+/**
+ * Steps through the game logic
+ */
 static void game_step() {
+  // Every step we want to keep the player moving and the background moving
+  player_step(s_graphics);
+  render_background(s_graphics, get_player_x(), get_player_y());
+  
   switch (s_game_state) {
+    // Animate the window flying in until it's done
     case GS_LOAD_IN_TRANSITION:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
-
       if (!is_window_animating()) {
         s_game_state = GS_NEW_GAME;
       } else {
         step_window_animation(s_graphics);
       }
       break;
-    case GS_NEW_GAME:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
-      break;
-    case GS_NEW_GAME_TRANSITION:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
 
+    // Wait for player input
+    case GS_NEW_GAME:
+      break;
+
+    // Once window is off the screen, load the items
+    case GS_NEW_GAME_TRANSITION:
       if (!is_window_animating()) {
         s_game_state = GS_PLAYING;
         items_init(s_graphics);
@@ -74,43 +89,49 @@ static void game_step() {
         step_window_animation(s_graphics);
       }
       break;
+
+    // Main gameplay state
     case GS_PLAYING:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
-      items_step(s_graphics);
+      items_step(s_graphics); // Move the items
 
-      check_collision();
+      check_collision(); // Handle player overlaps
 
+      // Draw the score bar on the top row of the background
       char top_banner_text[33];
       snprintf(top_banner_text, 32, "f      bx%d", s_player_score);
       clear_top_row(s_graphics);
       draw_text_at_location(s_graphics, top_banner_text, SCORE_BAR_OFFSET, 0, 0, true);
       
-      update_fuel();
+      update_fuel(); // Consume and draw the fuel
 
-      if (s_player_fuel == 0) {
+      // Once the player runs out of fuel, the game is over
+      if (s_player_fuel == 0) { 
         player_set_vertical_direction(D_NONE);
-        player_move_off_screen_down();
+        player_move_off_screen_down(); // Fall off the screen
         s_game_state = GS_MOVE_PLAYER_OFF_SCREEN;
+
+        // Check if we have a new high score
         s_new_high_score = s_player_score > s_high_score;
         if (s_new_high_score) {
           s_high_score = s_player_score;
         }
       }
       break;
+
+    // Move the player off the screen, and keep the items moving
     case GS_MOVE_PLAYER_OFF_SCREEN:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
       items_step(s_graphics);
 
+      // Once the player is off, slide the game over screen from the left
       if (!player_moving()) {
         char new_game_text[70];
+        // Display a different message if player lost (fuel out) or quit (back button)
         snprintf(new_game_text, 70, "%s\n\nSCORE:   %5d\n\nHI SCORE:%5d\n\n PRESS SELECT",
                  s_player_fuel == 0 ? " OUT OF FUEL" : " FLIGHT ENDED", s_player_score, s_high_score);
         clear_window_layer(s_graphics);
         set_window_layer_frame(s_graphics, GAME_OVER_WINDOW, GAME_OVER_WINDOW_START);
         draw_text_at_location(s_graphics, new_game_text, PBL_IF_ROUND_ELSE(4, 2), PBL_IF_ROUND_ELSE(3, 7), WINDOW_PALETTE, false);
-        set_window_has_priority(s_graphics, false);
+        set_window_has_priority(s_graphics, false); // Want to make sure the "NEW" sprite is visible
 
         start_window_animation(GAME_OVER_WINDOW_START, GAME_OVER_WINDOW_END);
 
@@ -121,64 +142,69 @@ static void game_step() {
       }
 
       break;
-    case GS_GAME_OVER_TRANSITION:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
 
+    // Game over screen moving on, items keep moving
+    case GS_GAME_OVER_TRANSITION:
       if (!is_window_animating()) {
         items_clear();
-        items_step(s_graphics);
         s_game_state = GS_GAME_OVER;
       } else {
         step_window_animation(s_graphics);
       }
+
       items_step(s_graphics);
-      if (s_new_high_score) {
+
+      if (s_new_high_score) { // Move high score sprite with it
         hiscore_step(s_graphics);
       }
       break;
+
+    // Wait for player input, keep high score sprite bobbing and changing color
     case GS_GAME_OVER:
       if (s_new_high_score) {
         hiscore_step(s_graphics);
       }
       break;
-    case GS_GAME_OVER_NEW_GAME_TRANSITION:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
-      items_step(s_graphics);
 
+    // Move game over screen off screen
+    case GS_GAME_OVER_NEW_GAME_TRANSITION:
       if (!is_window_animating()) {
         s_game_state = GS_MOVE_PLAYER_ON_SCREEN;
         player_move_on_screen_right();
       } else {
         step_window_animation(s_graphics);
       }
-      if (s_new_high_score) {
+
+      if (s_new_high_score) { // Move high score sprite with it
         hiscore_step(s_graphics);
       }
       break;
-    case GS_MOVE_PLAYER_ON_SCREEN:
-      player_step(s_graphics);
-      render_background(s_graphics, get_player_x(), get_player_y());
-      items_step(s_graphics);
 
+    // Move player onto the screen 
+    case GS_MOVE_PLAYER_ON_SCREEN:
       if (!player_moving()) {
         s_game_state = GS_PLAYING;
-        items_init(s_graphics);
+        items_init(s_graphics); // Reset the items here
       }
       break;
     default:
       break;
   }
 
-  GBC_Graphics_render(s_graphics);
+  GBC_Graphics_render(s_graphics); // Render the screen every step
 }
 
+/**
+ * The callback for the timer, this is where we call the game step
+ */
 static void frame_timer_handle(void* context) {
   s_frame_timer = app_timer_register(FRAME_DURATION, frame_timer_handle, NULL);
   game_step();
 }
 
+/**
+ * Handler for when the app loses or gains focus
+ */
 static void will_focus_handler(bool in_focus) {
   if (!in_focus) {
     // If a notification pops up while the timer is firing
@@ -194,9 +220,15 @@ static void will_focus_handler(bool in_focus) {
   }
 }
 
-// Initialize the GBC_Graphics object
+/**
+ * Initialize the GBC_Graphics object
+ * 
+ * @param window The window to render the GBC_Graphics object in
+ * @return A pointer to a GBC_Graphics object
+ */
 GBC_Graphics *init_gbc_graphics(Window *window) {
-  // Create with 1 vram bank since that's all the space we'll need
+  // Create with 1 VRAM bank since that's all the space we'll need
+  // Plus Aplite can only really support 1 bank
   GBC_Graphics *graphics = GBC_Graphics_ctor(window, 1);
 
   // I have predefined some common screen sizes, prefixed by SCREEN_BOUNDS_,
@@ -205,7 +237,7 @@ GBC_Graphics *init_gbc_graphics(Window *window) {
   // lead to some serious performance gains
 #if defined(PBL_ROUND)
   // For round, I want to make sure the score bar is visible, so I use
-  // the large screen bounds and scoot it down. In main.c, I set the
+  // the large screen bounds and move it down. In main.c, I set the
   // window color to the same blue as the sky, in order to give the
   // illusion of a fullscreen application
   GRect round_bounds = SCREEN_BOUNDS_LARGE;
@@ -229,79 +261,26 @@ GBC_Graphics *init_gbc_graphics(Window *window) {
   return graphics;
 }
 
+/**
+ * Starts a new game
+ */
 void new_game() {
-  generate_new_game_background(s_graphics);
+  // Setup the background and player
+  generate_new_game_background(s_graphics); // Create a new, randomized background
   render_background(s_graphics, get_player_x(), get_player_y());
   player_init(s_graphics);
+
+  // Reset the player stat variables
   s_player_fuel = MAX_PLAYER_FUEL;
   s_player_score = 0;
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
-  window_raw_click_subscribe(BUTTON_ID_UP, up_press_handler, up_release_handler, NULL);
-  window_raw_click_subscribe(BUTTON_ID_DOWN, down_press_handler, down_release_handler, NULL);
-}
-
-static bool load(SaveData *data) {
-  return persist_read_data(SAVE_KEY, data, sizeof(SaveData)) != E_DOES_NOT_EXIST;
-}
-
-static void save() {
-  if (s_player_score > s_high_score) {
-    s_high_score = s_player_score;
-  }
-  SaveData data = (SaveData) {
-    .high_score = s_high_score,
-  };
-  persist_write_data(SAVE_KEY, &data, sizeof(SaveData));
-}
-
-void game_init(Window *window) {
-  s_graphics = init_gbc_graphics(window);
-
-  SaveData data;
-  if (load(&data)) {
-    s_high_score = data.high_score;
-  }
-  
-  window_init(s_graphics);
-  text_init(s_graphics);
-  hiscore_init(s_graphics);
-  new_game();
-  player_move_on_screen_right();
-  
-  s_game_state = GS_LOAD_IN_TRANSITION;
-
-  clear_top_row(s_graphics);
-  draw_text_at_location(s_graphics, "START GAME", PBL_IF_ROUND_ELSE(6, 4), 0, 0, true);
-  
-  char new_game_text[33];
-  snprintf(new_game_text, 32, "HI SCORE:%5d\n\n PRESS SELECT", s_high_score);
-  clear_window_layer(s_graphics);
-  set_window_layer_frame(s_graphics, NEW_GAME_WINDOW, NEW_GAME_WINDOW_START);
-  draw_text_at_location(s_graphics, new_game_text, PBL_IF_ROUND_ELSE(4, 2), 1, WINDOW_PALETTE, false);
-  start_window_animation(NEW_GAME_WINDOW_START, NEW_GAME_WINDOW_END);
-  
-  window_set_click_config_provider(window, click_config_provider);
-
-  s_frame_timer = app_timer_register(FRAME_DURATION, frame_timer_handle, NULL);
-  app_focus_service_subscribe(will_focus_handler);
-
-  GBC_Graphics_render(s_graphics);
-}
-
-void game_deinit() {
-  // Save our high score
-  save();
-  // Be sure to destroy the graphic object when done!
-  GBC_Graphics_destroy(s_graphics);
-}
-
+/**
+ * Handles select clicks, for starting the game
+ */
 void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  // Do different things depending on the current state
   switch (s_game_state) {
+    // Exit new game menu, start the game
     case GS_NEW_GAME:
       clear_top_row(s_graphics);
       draw_text_at_location(s_graphics, "GET READY", PBL_IF_ROUND_ELSE(6, 4), 0, 0, true);
@@ -309,6 +288,8 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context) {
       s_game_state = GS_NEW_GAME_TRANSITION;
       s_new_high_score = false;
       break;
+
+    // Exit the game over menu, start a new game
     case GS_GAME_OVER:
       new_game();
       clear_top_row(s_graphics);
@@ -321,16 +302,24 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   }
 }
 
+/**
+ * Ends a round when by way of the back button press
+ */
 static void end_round() {
   player_set_vertical_direction(D_NONE);
-  player_move_off_screen_left();
+  player_move_off_screen_left(); // Slide off the scren
   s_game_state = GS_MOVE_PLAYER_OFF_SCREEN;
+
+  // Game is over, so check high score!
   s_new_high_score = s_player_score > s_high_score;
   if (s_new_high_score) {
     s_high_score = s_player_score;
   }
 }
 
+/**
+ * Handles back clicks, ending round when playing, quitting app when not playing
+ */
 void back_click_handler(ClickRecognizerRef recognizer, void *context) {
   if (s_game_state == GS_PLAYING) {
     end_round();
@@ -339,26 +328,131 @@ void back_click_handler(ClickRecognizerRef recognizer, void *context) {
   }
 }
 
+/**
+ * Handle up presses, moves player up while playing
+ */
 void up_press_handler(ClickRecognizerRef recognizer, void *context) {
   if (s_game_state == GS_PLAYING) {
     player_set_vertical_direction(D_UP);
   }
 }
 
+/**
+ * Handle up releases, stops player movement while playing
+ */
 void up_release_handler(ClickRecognizerRef recognizer, void *context) {
   if (s_game_state == GS_PLAYING) {
     player_set_vertical_direction(D_NONE);
   }
 }
 
+/**
+ * Handle down presses, moves player down while playing
+ */
 void down_press_handler(ClickRecognizerRef recognizer, void *context) {
   if (s_game_state == GS_PLAYING) {
     player_set_vertical_direction(D_DOWN);
   }
 }
 
+/**
+ * Handle down releases, stops player movement while playing
+ */
 void down_release_handler(ClickRecognizerRef recognizer, void *context) {
   if (s_game_state == GS_PLAYING) {
     player_set_vertical_direction(D_NONE);
   }
+}
+
+/**
+ * Setup input callbacks
+ */
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
+  window_raw_click_subscribe(BUTTON_ID_UP, up_press_handler, up_release_handler, NULL);
+  window_raw_click_subscribe(BUTTON_ID_DOWN, down_press_handler, down_release_handler, NULL);
+}
+
+/**
+ * Load prevously saved data
+ * 
+ * @param data A pointer to a SaveData object to load into
+ * @return true if previous save data was found, false if no previous save
+ */
+static bool load(SaveData *data) {
+  return persist_read_data(SAVE_KEY, data, sizeof(SaveData)) != E_DOES_NOT_EXIST;
+}
+
+/**
+ * Saves the player's high score
+ */
+static void save() {
+  // Compare high score in case the player beat it
+  if (s_player_score > s_high_score) {
+    s_high_score = s_player_score;
+  }
+
+  SaveData data = (SaveData) {
+    .high_score = s_high_score,
+  };
+  persist_write_data(SAVE_KEY, &data, sizeof(SaveData));
+}
+
+/**
+ * Create the game
+ * 
+ * @param window The window to render the GBC_Graphics object in
+ */
+void game_init(Window *window) {
+  s_graphics = init_gbc_graphics(window);
+
+  // Load the previous high score if it exists
+  SaveData data;
+  if (load(&data)) {
+    s_high_score = data.high_score;
+  }
+  
+  // Setup the game
+  window_init(s_graphics);
+  text_init(s_graphics);
+  hiscore_init(s_graphics);
+  new_game();
+
+  // Draw top bar text
+  clear_top_row(s_graphics);
+  draw_text_at_location(s_graphics, "START GAME", PBL_IF_ROUND_ELSE(6, 4), 0, 0, true);
+  
+  // Set up the new game window
+  char new_game_text[33];
+  snprintf(new_game_text, 32, "HI SCORE:%5d\n\n PRESS SELECT", s_high_score);
+  clear_window_layer(s_graphics);
+  set_window_layer_frame(s_graphics, NEW_GAME_WINDOW, NEW_GAME_WINDOW_START);
+  draw_text_at_location(s_graphics, new_game_text, PBL_IF_ROUND_ELSE(4, 2), 1, WINDOW_PALETTE, false);
+
+
+  // Move the player on from the left, and move the window on from the bottom
+  player_move_on_screen_right();
+  start_window_animation(NEW_GAME_WINDOW_START, NEW_GAME_WINDOW_END);
+  s_game_state = GS_LOAD_IN_TRANSITION;
+  
+  // Setup input
+  window_set_click_config_provider(window, click_config_provider);
+
+  // Setup the frame timer that will call the game step function
+  s_frame_timer = app_timer_register(FRAME_DURATION, frame_timer_handle, NULL);
+  app_focus_service_subscribe(will_focus_handler);
+
+  // Do an initial render of our input
+  GBC_Graphics_render(s_graphics);
+}
+
+/**
+ * Destroy the game
+ */
+void game_deinit() {
+  // Save the high score before exiting
+  save();
+  // Be sure to destroy the graphic object when done!
+  GBC_Graphics_destroy(s_graphics);
 }
--- pebble-gbc-graphics/src/c/game.h
+++ pebble-gbc-graphics/src/c/game.h
@@ -3,61 +3,66 @@
 #include <pebble.h>
 #include "pebble-gbc-graphics/pebble-gbc-graphics.h"
 
-// This is the duration of a frame, in milliseconds
-// E.g. a FRAME_DURATION of 33ms will give ~30 fps
-// For "as fast as possible" (i.e. render as soon as the
-// graphics engine has completed its last draw cycle),
-// use a FRAME_DURATION of 2. Keep in mind that black
-// and white pebbles will render much faster than the
-// color pebbles. I manually tested different values for
-// the non-color frame duration to find one that matched the
-// color fps, giving a consistent experience across all pebbles
+/*
+ * This is the duration of a frame, in milliseconds (more or less)
+ * E.g. a FRAME_DURATION of 33ms will give ~30 fps
+ * For "as fast as possible" (i.e. render as soon as the
+ * graphics engine has completed its last draw cycle),
+ * use a FRAME_DURATION of 2. 
+ * Black and white Pebbles tend to behave differently than color
+ * Pebbles, so you may have to play around with these values to 
+ * find one that works best for your application
+ */
 #if defined(PBL_COLOR)
 #define FRAME_DURATION 50
 #else
 #define FRAME_DURATION 20
 #endif
 
-#define MAX_PLAYER_FUEL 500
-#define NUM_FUEL_BARS 5
-#define SCORE_BAR_OFFSET PBL_IF_ROUND_ELSE(5, 2)
+#define MAX_PLAYER_FUEL 500 // The amount of fuel the player has
+#define NUM_FUEL_BARS 5 // The number of fuel bars to render the fuel into
+#define SCORE_BAR_OFFSET PBL_IF_ROUND_ELSE(5, 2) // The x offset of the score bar
 
-#define NEW_GAME_WINDOW GRect(PBL_IF_ROUND_ELSE(3, 1), 0, 15, 4)
-#define NEW_GAME_WINDOW_END GPoint(0, GBC_Graphics_get_screen_height(s_graphics) - 48)
-#define NEW_GAME_WINDOW_START GPoint(0, GBC_Graphics_get_screen_height(s_graphics))
+#define NEW_GAME_WINDOW GRect(PBL_IF_ROUND_ELSE(3, 1), 0, 15, 4) // The frame for the New Game window
+#define NEW_GAME_WINDOW_START GPoint(0, GBC_Graphics_get_screen_height(s_graphics)) // The start position of the New Game window animation
+#define NEW_GAME_WINDOW_END GPoint(0, GBC_Graphics_get_screen_height(s_graphics) - 48) // The end position of the New Game window animation
 
-#if defined(PBL_ROUND)
-    #define GAME_OVER_WINDOW GRect(3, 2, 15, 8)
-#else
-    #define GAME_OVER_WINDOW GRect(1, 6, 15, 8)
-#endif
-#define GAME_OVER_WINDOW_START GPoint(GBC_Graphics_get_screen_width(s_graphics), 8)
-#define GAME_OVER_WINDOW_END GPoint(0, 8)
+#define GAME_OVER_WINDOW GRect(PBL_IF_ROUND_ELSE(3, 1), PBL_IF_ROUND_ELSE(2, 6), 15, 8) // The frame for the Game Over window
+#define GAME_OVER_WINDOW_START GPoint(GBC_Graphics_get_screen_width(s_graphics), 8) // The start position of the Game Over window animation
+#define GAME_OVER_WINDOW_END GPoint(0, 8) // The end position of the Game Over window animation
 
-#define SAVE_KEY 0
+#define SAVE_KEY 0 // The key for the save file
 
+/**
+ * The save data structure
+ */
 typedef struct {
-    uint16_t high_score;
+    uint16_t high_score; // The player's high score
 } SaveData;
 
+/**
+ * The states of the game
+ */
 typedef enum {
-    GS_LOAD_IN_TRANSITION,
-    GS_NEW_GAME,
-    GS_NEW_GAME_TRANSITION,
-    GS_PLAYING,
-    GS_MOVE_PLAYER_OFF_SCREEN,
-    GS_GAME_OVER_TRANSITION,
-    GS_GAME_OVER,
-    GS_GAME_OVER_NEW_GAME_TRANSITION,
-    GS_MOVE_PLAYER_ON_SCREEN,
+    GS_LOAD_IN_TRANSITION,              // Load in, new game window and player fly in
+    GS_NEW_GAME,                        // Waiting for player input to start
+    GS_NEW_GAME_TRANSITION,             // Hiding new game window
+    GS_PLAYING,                         // Main gameplay state
+    GS_MOVE_PLAYER_OFF_SCREEN,          // Game ended, player moving off screen
+    GS_GAME_OVER_TRANSITION,            // Game over window fly in
+    GS_GAME_OVER,                       // Waiting for player input to start again
+    GS_GAME_OVER_NEW_GAME_TRANSITION,   // Game over window fly out
+    GS_MOVE_PLAYER_ON_SCREEN,           // Player fly in
 } GameState;
 
+/**
+ * Create a new game
+ * 
+ * @param window The window that the graphics should render into
+ */
 void game_init(Window *window);
-void game_deinit();
 
-void select_click_handler(ClickRecognizerRef recognizer, void *context);
-void back_click_handler(ClickRecognizerRef recognizer, void *context);
-void up_press_handler(ClickRecognizerRef recognizer, void *context);
-void up_release_handler(ClickRecognizerRef recognizer, void *context);
-void down_press_handler(ClickRecognizerRef recognizer, void *context);
-void down_release_handler(ClickRecognizerRef recognizer, void *context);
+/**
+ * Destroy a game session
+ */
+void game_deinit();
