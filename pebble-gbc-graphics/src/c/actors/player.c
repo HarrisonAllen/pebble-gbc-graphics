b1936b30fb74a9909b7d8a7e39a77893b1c638c2
Harrison Allen <hmallen@mit.edu>
2021-07-20 12:08:09 -0400

    Documented

--- pebble-gbc-graphics/src/c/actors/player.c
+++ pebble-gbc-graphics/src/c/actors/player.c
@@ -1,22 +1,29 @@
 #include "player.h"
 #include "../util.h"
 
-static uint s_player_x;
-static uint8_t s_player_y;
-static Direction s_player_y_direction;
-static uint8_t s_player_x_speed;
-static int8_t s_player_y_speed;
-static uint8_t s_prop_frame, s_bob_frame, s_slide_frame;
-static int s_player_screen_x, s_player_screen_y_offset;
-static PlayerState s_player_state;
-static EngineSpeed s_engine_speed;
+static uint32_t s_player_x; // The player's x position in the world
+static uint8_t s_player_y; // The player's y position in the world
+static uint8_t s_player_x_speed; // The player's horizontal speed
+static int8_t s_player_y_speed; // The player's vertical velocity
+static int s_player_screen_x; // The player's x position on the screen, for animations
+static int s_player_screen_y_offset; // The player's y position offset from the screen position, for animations
+static Direction s_player_y_direction; // The player's vertical direction
 
-extern uint8_t sprites_vram_offset;
+static uint8_t s_prop_frame; // The animation frame for the propellor
+static uint8_t s_bob_frame; // The animation frame for the up and down bobbing
+static uint8_t s_slide_frame; // The animation frame for sliding in and out
 
-extern const uint8_t ease_in_out_percentages[];
+static PlayerState s_player_state; // The player's state
+static EngineSpeed s_engine_speed; // The speed of the engine
 
-extern const uint8_t accelerate_slow_percentages[];
+extern uint8_t sprites_vram_offset; // Need to grab the offset of the sprites tilesheet
+extern const uint8_t ease_in_out_percentages[]; // Want to grab the ease in, ease out animation sequence
+extern const uint8_t accelerate_slow_percentages[]; // Also want the slow acceleration animation sequence
 
+/**
+ * Custom animation sequence for bobbing up and down,
+ * centered around zero
+ */
 static const int8_t s_bob_offsets[] = {
     0,
     -1,
@@ -38,27 +45,31 @@ static const int8_t s_bob_offsets[] = {
     1,
 };
 
+// The propellor frames to use for a fast engine speed
 static const uint8_t s_prop_fast_frames[] = {
-    PLANE_NOSE_0,
-    PLANE_NOSE_3,
+    PLANE_NOSE_0_SPRITE,
+    PLANE_NOSE_3_SPRITE,
 };
 
+// The propellor frames to use for a medium engine speed
 static const uint8_t s_prop_medium_frames[] = {
-    PLANE_NOSE_0,
-    PLANE_NOSE_2,
-    PLANE_NOSE_3,
-    PLANE_NOSE_1,
+    PLANE_NOSE_0_SPRITE,
+    PLANE_NOSE_2_SPRITE,
+    PLANE_NOSE_3_SPRITE,
+    PLANE_NOSE_1_SPRITE,
 };
 
+// The propellor frames to use for a slow engine speed
 static const uint8_t s_prop_slow_frames[] = {
-    PLANE_NOSE_0,
-    PLANE_NOSE_1,
-    PLANE_NOSE_2,
-    PLANE_NOSE_3,
-    PLANE_NOSE_2,
-    PLANE_NOSE_1,
+    PLANE_NOSE_0_SPRITE,
+    PLANE_NOSE_1_SPRITE,
+    PLANE_NOSE_2_SPRITE,
+    PLANE_NOSE_3_SPRITE,
+    PLANE_NOSE_2_SPRITE,
+    PLANE_NOSE_1_SPRITE,
 };
 
+// Convert an engine speed to a list of propellor frames
 static const uint8_t *s_engine_speed_to_prop_frames[] = {
     s_prop_fast_frames,
     s_prop_medium_frames,
@@ -66,12 +77,19 @@ static const uint8_t *s_engine_speed_to_prop_frames[] = {
     s_prop_slow_frames,
 };
 
+// How many frame are in the prop frames for that engine speed
 static const uint8_t s_num_prop_frames_per_speed[] = {
     2, 4, 6, 6
 };
 
 static void set_player_sprite_palette(GBC_Graphics *graphics) {
-    // The first color in a sprite palette is treated as transparency, and won't be visible
+    /**
+     * The colors are:
+     *   1. Transparent (doesn't matter)
+     *   2. Border color
+     *   3. Primary color
+     *   4. Detail color
+     */
 #if defined(PBL_COLOR)
     GBC_Graphics_set_sprite_palette(graphics, PLAYER_SPRITE_PALETTE, GColorPictonBlueARGB8, GColorBlackARGB8, GColorRedARGB8, GColorWhiteARGB8);
 #else
@@ -79,52 +97,64 @@ static void set_player_sprite_palette(GBC_Graphics *graphics) {
 #endif
 }
 
+/**
+ * Renders the player's sprites
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ */
 static void draw_player_sprites(GBC_Graphics *graphics) {
     uint8_t plane_attrs = GBC_Graphics_attr_make(PLAYER_SPRITE_PALETTE, 0, false, false, false);
+
+    // Set the player y (relative to the background) and offset by the bobbing animation
     uint8_t player_screen_y = clamp_int(0, s_player_y - GBC_Graphics_bg_get_scroll_y(graphics) + GBC_SPRITE_OFFSET_Y + s_player_screen_y_offset, UINT8_MAX);
-    
     player_screen_y += s_bob_offsets[s_bob_frame / (s_engine_speed + 1)];
 
-    GBC_Graphics_oam_set_sprite(graphics, 0, clamp_int(0, s_player_screen_x, s_player_screen_x), player_screen_y, sprites_vram_offset + PLANE_RUDDER, plane_attrs);
+    // Draw the rudder
+    GBC_Graphics_oam_set_sprite(graphics, 0, clamp_int(0, s_player_screen_x, s_player_screen_x), player_screen_y, sprites_vram_offset + PLANE_RUDDER_SPRITE, plane_attrs);
 
-    GBC_Graphics_oam_set_sprite(graphics, 1, clamp_int(0, s_player_screen_x + GBC_TILE_WIDTH, s_player_screen_x + GBC_TILE_WIDTH), player_screen_y, sprites_vram_offset + PLANE_TAIL, plane_attrs);
+    // Draw the tail
+    GBC_Graphics_oam_set_sprite(graphics, 1, clamp_int(0, s_player_screen_x + GBC_TILE_WIDTH, s_player_screen_x + GBC_TILE_WIDTH), player_screen_y, sprites_vram_offset + PLANE_TAIL_SPRITE, plane_attrs);
 
+    // Based on player direction, draw the body
     uint8_t plane_body_sprite;
     switch (s_player_y_direction) {
         case D_UP:
-            plane_body_sprite = PLANE_BODY_UP;
+            plane_body_sprite = PLANE_BODY_UP_SPRITE;
             break;
         case D_DOWN:
-            plane_body_sprite = PLANE_BODY_DOWN;
+            plane_body_sprite = PLANE_BODY_DOWN_SPRITE;
             break;
         default:
-            plane_body_sprite = PLANE_BODY;
+            plane_body_sprite = PLANE_BODY_SPRITE;
             break;
     }
     GBC_Graphics_oam_set_sprite(graphics, 2, clamp_int(0, s_player_screen_x + GBC_TILE_WIDTH * 2, s_player_screen_x + GBC_TILE_WIDTH * 2), player_screen_y, sprites_vram_offset + plane_body_sprite, plane_attrs);
 
+    // Based on engine speed and the propellor frame, draw the nose
     uint8_t plane_engine_sprite = s_engine_speed_to_prop_frames[s_engine_speed][s_prop_frame];
     GBC_Graphics_oam_set_sprite(graphics, 3, clamp_int(0, s_player_screen_x + GBC_TILE_WIDTH * 3, s_player_screen_x + GBC_TILE_WIDTH * 3), player_screen_y, sprites_vram_offset + plane_engine_sprite, plane_attrs);
 }
 
 void player_init(GBC_Graphics *graphics) {
     s_player_x = 0;
-    s_player_y = 80;
-    s_player_x_speed = 4;
+    s_player_y = PLAYER_START_Y;
+    s_player_x_speed = PLAYER_X_SPEED;
     s_player_y_speed = 0;
     s_player_screen_x = PLAYER_OFF_SCREEN_X;
     s_player_screen_y_offset = 0;
+    s_player_y_direction = D_NONE;
     s_player_state = PS_OFF_SCREEN;
     s_engine_speed = ES_FAST;
-    s_player_y_direction = D_NONE;
     set_player_sprite_palette(graphics);
     draw_player_sprites(graphics);
 }
 
 void player_step(GBC_Graphics *graphics) {
+    // Move the player to the right through the world
     s_player_x += s_player_x_speed;
 
     switch (s_player_state) {
+        // Playing the game, so move up and down based on user inputs
         case PS_ON_SCREEN: 
             if (s_player_y_direction == D_NONE && s_player_y_speed != 0) {
                 s_player_y_speed -= 1 * abs(s_player_y_speed) / s_player_y_speed; // just a trick to normalize an integer
@@ -133,6 +163,8 @@ void player_step(GBC_Graphics *graphics) {
             }
             s_player_y = clamp_uint8_t(MIN_PLAYER_Y, s_player_y + s_player_y_speed, MAX_PLAYER_Y);
             break;
+
+        // Ease the player off the screen to the left
         case PS_MOVING_OFF_LEFT: {
             uint8_t animation_percent = ease_in_out_percentages[s_slide_frame];
             s_player_screen_x = lerp(PLAYER_ON_SCREEN_X, PLAYER_OFF_SCREEN_X, animation_percent);
@@ -141,17 +173,23 @@ void player_step(GBC_Graphics *graphics) {
                 s_player_state = PS_OFF_SCREEN;
             }
         } break;
-        case PS_MOVING_OFF_DOWN: {
+
+        // Accelerate the player off the screen downwards (falling) and ease off to the left (slowing)
+        case PS_MOVING_OFF_DOWN_AND_LEFT: {
             uint8_t fall_animation_percent = accelerate_slow_percentages[s_slide_frame];
-            uint8_t slide_animation_percent = accelerate_slow_percentages[s_slide_frame];
             s_player_screen_y_offset = lerp(0, PLAYER_OFF_SCREEN_Y, fall_animation_percent);
+            
+            uint8_t slide_animation_percent = accelerate_slow_percentages[s_slide_frame];
             s_player_screen_x = lerp(PLAYER_ON_SCREEN_X, PLAYER_OFF_SCREEN_X, slide_animation_percent);
             s_slide_frame ++;
+
             if (fall_animation_percent == 100 || slide_animation_percent == 100) {
                 s_player_state = PS_OFF_SCREEN;
             }
         } break;
-        case PS_MOVING_ON_RIGHT: {
+
+        // Ease the player on the screen from the left
+        case PS_MOVING_ON_LEFT: {
             uint8_t animation_percent = ease_in_out_percentages[s_slide_frame];
             s_player_screen_x = lerp(PLAYER_OFF_SCREEN_X, PLAYER_ON_SCREEN_X, animation_percent);
             s_slide_frame ++;
@@ -163,14 +201,17 @@ void player_step(GBC_Graphics *graphics) {
             break;
     }
 
+    // Don't forget to render
     draw_player_sprites(graphics);
+
+    // Keep the propellor and bobbing animations going
     if (s_engine_speed != ES_STOPPED) {
         s_prop_frame = (s_prop_frame + 1) % s_num_prop_frames_per_speed[s_engine_speed];
         s_bob_frame = (s_bob_frame + 1) % (sizeof(s_bob_offsets) * (s_engine_speed + 1));
     }
 }
 
-uint get_player_x() {
+uint32_t get_player_x() {
     return s_player_x;
 }
 
@@ -189,11 +230,11 @@ uint8_t get_player_screen_y(GBC_Graphics *graphics) {
 }
 
 GRect get_player_collision() {
-    return GRect(s_player_x + 20, s_player_y + s_bob_offsets[s_bob_frame / (s_engine_speed + 1)] + 7, 8, 3);
+    return GRect(s_player_x + 20, s_player_y + s_bob_offsets[s_bob_frame / (s_engine_speed + 1)] + 7, 8, 3); // Small rectangle around the body/nose
 }
 
-void player_set_vertical_direction(Direction new_scroll_dir) {
-  s_player_y_direction = new_scroll_dir;
+void player_set_vertical_direction(Direction new_dir) {
+  s_player_y_direction = new_dir;
 }
 
 void player_move_off_screen_left() {
@@ -201,26 +242,26 @@ void player_move_off_screen_left() {
     s_slide_frame = 0;
 }
 
-void player_move_on_screen_right() {
-    s_player_state = PS_MOVING_ON_RIGHT;
+void player_move_on_screen_left() {
+    s_player_state = PS_MOVING_ON_LEFT;
     s_slide_frame = 0;
 }
 
 void player_move_off_screen_down() {
-    s_player_state = PS_MOVING_OFF_DOWN;
+    s_player_state = PS_MOVING_OFF_DOWN_AND_LEFT;
     s_slide_frame = 0;
 }
 
-bool player_moving() {
-    return s_player_state == PS_MOVING_OFF_LEFT || s_player_state == PS_MOVING_ON_RIGHT || s_player_state == PS_MOVING_OFF_DOWN;
+bool is_player_moving() {
+    return s_player_state == PS_MOVING_OFF_LEFT || s_player_state == PS_MOVING_ON_LEFT || s_player_state == PS_MOVING_OFF_DOWN_AND_LEFT;
 }
 
-bool player_on_screen() {
+bool is_player_on_screen() {
     return s_player_state == PS_ON_SCREEN;
 }
 
 void player_set_engine_speed(GBC_Graphics *graphics, EngineSpeed new_speed) {
-    s_bob_frame = s_bob_frame * (new_speed + 1) / (s_engine_speed + 1);
+    s_bob_frame = s_bob_frame * (new_speed + 1) / (s_engine_speed + 1); // Make sure the new bob frame lines up with the old one
     s_engine_speed = new_speed;
-    s_prop_frame %= s_num_prop_frames_per_speed[s_engine_speed];
+    s_prop_frame %= s_num_prop_frames_per_speed[s_engine_speed]; // Constrain the prop frame to the current number of frames
 }
--- pebble-gbc-graphics/src/c/actors/player.h
+++ pebble-gbc-graphics/src/c/actors/player.h
@@ -4,50 +4,138 @@
 #include "../pebble-gbc-graphics/pebble-gbc-graphics.h"
 #include "../util.h"
 
-#define MIN_PLAYER_Y 8
-#define MAX_PLAYER_Y 208
-#define MAX_PLAYER_Y_SPEED 6
-#define PLAYER_ON_SCREEN_X PBL_IF_ROUND_ELSE(32, 16)
-#define PLAYER_OFF_SCREEN_X -32
-#define PLAYER_OFF_SCREEN_Y GBC_Graphics_get_screen_height(graphics)
+#define MIN_PLAYER_Y 8 // The highest position that the player can go
+#define MAX_PLAYER_Y 208 // The lowest position that the player can go
+#define MAX_PLAYER_Y_SPEED 6 // The fastest that the player can move vertically
+#define PLAYER_START_Y 80 // The starting y position for the player
+#define PLAYER_X_SPEED 4 // The player's horizontal speed
 
-#define PLAYER_SPRITE_PALETTE 0
-#define PLANE_RUDDER 0
-#define PLANE_TAIL 2
-#define PLANE_BODY 4
-#define PLANE_BODY_UP 6
-#define PLANE_BODY_DOWN 8
-#define PLANE_NOSE_0 10
-#define PLANE_NOSE_1 12
-#define PLANE_NOSE_2 14
-#define PLANE_NOSE_3 16
+#define PLAYER_ON_SCREEN_X PBL_IF_ROUND_ELSE(32, 16) // The x position of the player when on scren
+#define PLAYER_OFF_SCREEN_X -32 // The x position of the player when off screen left
+#define PLAYER_OFF_SCREEN_Y GBC_Graphics_get_screen_height(graphics) // The y position of the player when off screen down
 
+#define PLANE_RUDDER_SPRITE 0 // The tilesheet offset for the rudder (1st sprite)
+#define PLANE_TAIL_SPRITE 2 // The tilesheet offset for the tail (2nd sprite)
+#define PLANE_BODY_SPRITE 4 // The tilesheet offset for the body when going straight (3rd sprite)
+#define PLANE_BODY_UP_SPRITE 6 // The tilesheet offset for the body when going up (3rd sprite)
+#define PLANE_BODY_DOWN_SPRITE 8 // The tilesheet offset for the body when going down (3rd sprite)
+#define PLANE_NOSE_0_SPRITE 10 // The tilesheet offset for nose, propellor vertical (4th sprite)
+#define PLANE_NOSE_1_SPRITE 12 // The tilesheet offset for nose, propellor mostly vertical (4th sprite)
+#define PLANE_NOSE_2_SPRITE 14 // The tilesheet offset for nose, propellor mostly horizontal (4th sprite)
+#define PLANE_NOSE_3_SPRITE 16 // The tilesheet offset for nose, propellor horizontal (4th sprite)
+#define PLAYER_SPRITE_PALETTE 0 // The sprite palette number for the plane sprites
+
+/**
+ * The state of the player
+ */
 typedef enum {
-    PS_ON_SCREEN,
-    PS_OFF_SCREEN,
-    PS_MOVING_OFF_LEFT,
-    PS_MOVING_ON_RIGHT,
-    PS_MOVING_OFF_DOWN
+    PS_ON_SCREEN,               // The player is on-screen
+    PS_OFF_SCREEN,              // The player is off-screen
+    PS_MOVING_OFF_LEFT,         // The player is moving off of the screen to the left
+    PS_MOVING_ON_LEFT,          // The player is moving on to the screen from the left
+    PS_MOVING_OFF_DOWN_AND_LEFT // The player is moving off of the screen downwards and to the left
 } PlayerState;
 
+/**
+ * The speed of the engine
+ */
 typedef enum {
-    ES_FAST,
-    ES_MEDIUM,
-    ES_SLOW,
-    ES_STOPPED,
+    ES_FAST,    // Fast propellor spin, fast bobbing
+    ES_MEDIUM,  // Medium propellor spin, medium bobbing
+    ES_SLOW,    // Slow propellor spin, slow bobbing
+    ES_STOPPED, // No propellor spin, no bobbing
 } EngineSpeed;
 
+/**
+ * Sets up the player
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ */
 void player_init(GBC_Graphics *graphics);
+
+/**
+ * Steps through the player's game logic
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ */
 void player_step(GBC_Graphics *graphics);
-uint get_player_x();
+
+/**
+ * Get the player's x position in the world
+ * 
+ * @return The player's x position in the world
+ */
+uint32_t get_player_x();
+
+/**
+ * Get the player's y position in the world
+ * 
+ * @return The player's y position in the world
+ */
 uint8_t get_player_y();
+
+/**
+ * Get the player's x position on the screen
+ * 
+ * @return The player's x position on the screen
+ */
 uint8_t get_player_screen_x();
+
+/**
+ * Get the player's y position on the screen
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ * @return The player's y position on the screen
+ */
 uint8_t get_player_screen_y(GBC_Graphics *graphics);
+
+/**
+ * Get the bounding rectangle that defines collision for the player
+ * 
+ * @return The GRect that defines the collision for the player
+ */
 GRect get_player_collision();
-void player_set_vertical_direction(Direction new_scroll_dir);
+
+/**
+ * Set the vertical direction of the player
+ * 
+ * @param new_dir The new vertical direction
+ */
+void player_set_vertical_direction(Direction new_dir);
+
+/**
+ * Start moving the player off of the screen to the left
+ */
 void player_move_off_screen_left();
-void player_move_on_screen_right();
+
+/**
+ * Start moving the player on to the screen from the left
+ */
+void player_move_on_screen_left();
+
+/**
+ * Start moving the player off of the screen downwards
+ */
 void player_move_off_screen_down();
-bool player_moving();
-bool player_on_screen();
+
+/**
+ * Check if the player is currently moving on or off the screen
+ *
+ * @return Whether or not the player is moving on or off the screen
+ */
+bool is_player_moving();
+
+/**
+ * Check if the player is currently on the screen and not moving
+ *
+ * @return Whether or not the player is on the screen and not moving
+ */
+bool is_player_on_screen();
+
+/**
+ * Sets the engine speed
+ * 
+ * @param graphics The GBC_Graphics object for rendering
+ * @param new_speed The new engine speed
+ */
 void player_set_engine_speed(GBC_Graphics *graphics, EngineSpeed new_speed);
